e79453164ab0b9e3ba13f4103767024def763784
Rafael <fconrafael@gmail.com>
2025-02-19 16:18:42 -0300

    alterações editar

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ function ocultarOverlay() {
     let modalBG = document.querySelector("#overlay");
     modalBG.classList.add("invisible", "opacity-0");
     ocultarSideBar()
+    ocultarSideBarEditar()
 }
 
 function mostrarSideBar() {
@@ -22,33 +23,47 @@ function ocultarSideBar() {
     modalBG.classList.add("-right-full");
 }
 
-function adicionarUsuario() {
+// function adicionarUsuario() {
+//     event.preventDefault();
+//     let formulario = document.querySelector("#criar");
+//     let formData = new FormData(formulario);
+//     let data = {};
+//     for(const pair of formData.entries()){
+//         data[pair[0]] = pair[1]
+//     }
+//     fetch("http://localhost:8000/usuarios", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "multipart/form-data"
+//         },
+//         body: data
+//     })
+//     .then(resposta => resposta.json())
+//     .then(resposta => {
+//         dados = resposta
+//     });
+// }
+
+function adicionarUsuario(event) {
     event.preventDefault();
-
-    // let nome = document.getElementById("nome").value;
-    // let email = document.getElementById("email").value;
-
     let formulario = document.querySelector("#criar");
     let formData = new FormData(formulario);
-    let data = {};
-    for(const pair of formData.entries()){
-        data[pair[0]] = pair[1]
-    }
+    let data = Object.fromEntries(formData.entries()); //converter um objeto FormData para um objeto // formData.entries() retorna um iterador contendo os pares chave-valor // Object.fromEntries() converte em um objeto
+    // let data = {};
+    // formData.forEach((value, key) => {  //converte em um objeto, pois náo pode ser diretametno transformado em JSON
+    //     data[key] = value;
+    // });
 
     fetch("http://localhost:8000/usuarios", {
         method: "POST",
         headers: {
-            "Content-Type": "multipart/form-data"
+            "Content-Type": "application/json"
         },
-        body: data
+        body: JSON.stringify(data) // enviar os dados corretamente na requisição POST
     })
     .then(resposta => resposta.json())
-    .then(resposta => {
-        dados = resposta
-    
-    });
-
-
+    .then(resposta => {alert("Usuário cadastrado com sucesso")
+    })
 }
 
 function buscarUsuarios(){
@@ -58,18 +73,42 @@ function buscarUsuarios(){
 }
 buscarUsuarios()
 
-function editarUsuario(){
-    // requisição do tipo PUT pra buscar os usuarios
+function mostrarSideBarEditar() {
+    let modalBG = document.querySelector("#sidebarEditar");
+    modalBG.classList.remove("-right-full");
+    modalBG.classList.add("right-0");
+    mostrarOverlay()
+}
+function ocultarSideBarEditar() {
+    let modalBG = document.querySelector("#sidebarEditar");
+    modalBG.classList.remove("right-0");
+    modalBG.classList.add("-right-full");
+}
+
+function editarUsuario(id){
+    fetch(`http://localhost:8000/usuarios/${id}`)
+    .then(resposta => resposta.json())
+    .then(resposta => {console.log(resposta);
+    });
+
+    document.getElementById("nomeEditar").value = resposta.nome;
+    document.getElementById("emailEditar").value = resposta.email;
+
 }
 
 function deletarUsuario(id){
+    let confirma = confirm("Tem certeza que deseja excluir o usuário");
+    if (confirma)
     fetch(`http://localhost:8000/usuarios/${id}`, {
         method: "DELETE",
     })
     .then(resposta => resposta.json())
-    .then(resposta => {alert("Usuario deletado com sucesso")});
+    .then(resposta => {alert("Usuário deletado com sucesso")});
+    else {
+        alert("Usuário não deletado");
+        return(listarUsuarios());
+    }
 }
-
 
 function listarUsuarios(usuarios){
     let table = document.querySelector("#usuarios");
@@ -80,7 +119,7 @@ function listarUsuarios(usuarios){
                 <td class="w-[250px]">${usuario.nome}</td>
                 <td class="w-full">${usuario.email}</td>
                 <td class="w-[100px] flex gap-3">
-                    <button class="w-[36px] h-[36px] p-2 flex justify-center items-center bg-verde-claro rounded-full hover:bg-verde-escuro group duration-200">
+                    <button onclick="editarUsuario('2')" class="w-[36px] h-[36px] p-2 flex justify-center items-center bg-verde-claro rounded-full hover:bg-verde-escuro group duration-200">
                         <box-icon type='solid' name='pencil' class="fill-verde-escuro group-hover:fill-verde-claro"></box-icon>
                     </button>
                     <button onclick="deletarUsuario(${usuario.id})" class="w-[36px] h-[36px] p-2 flex justify-center items-center bg-verde-claro rounded-full hover:bg-verde-escuro group duration-200">
